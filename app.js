193863b0950c61ab588c468c8fe54c501194453e
백종열 <geeksbaek@gmail.com>
2014-11-22 20:09:25 +0900

    ...

--- app.js
+++ app.js
@@ -61,6 +61,21 @@ template.onResponse = function(e, detail, sender) {
 
 })();
 
+function toggleFullScreen() {
+  var doc = window.document;
+  var docEl = doc.documentElement;
+      
+  var requestFullScreen = docEl.requestFullscreen || docEl.mozRequestFullScreen || docEl.webkitRequestFullScreen;
+  var cancelFullScreen = doc.exitFullscreen || doc.mozCancelFullScreen || doc.webkitExitFullscreen;
+      
+  if(!doc.fullscreenElement && !doc.mozFullScreenElement && !doc.webkitFullscreenElement) { 
+    requestFullScreen.call(docEl);
+  }
+  else {
+    cancelFullScreen.call(doc);  
+  }
+}
+
 PolymerExpressions.prototype.slice = function (v, n) {
   return v.slice(0, n) + (v.length > n ? "..." : "");
 };
